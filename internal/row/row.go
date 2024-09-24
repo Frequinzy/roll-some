b4f74b3bcade64e2433b240d4bd70408fd7a7bc9
Emil Hultcrantz <emilhtz@gmail.com>
2024-09-24 22:27:15 +0200

    Refactor some internal logic in rows

--- internal/row/row.go
+++ internal/row/row.go
@@ -9,28 +9,59 @@ import (
 
 type die struct {
 	sideCount int
+	value int
 }
 
-func (d *die)Roll() int {
-	return 1 + rand.Intn(d.sideCount)
+func (d *die)Roll() {
+	d.value = 1 + rand.Intn(d.sideCount)
 }
 
 type row struct {
 	dice []die
+	modifier int
 }
 
-func (r *row)Roll() int {
 	sum := 0
+func ParseRow(s string) (*row, error) {
+	var row row
+	for _, diceGroup := range strings.Fields(s) {
+		re := regexp.MustCompile(`(\d*)d(\d+)`)
+		matches := re.FindStringSubmatch(diceGroup)
+		dieCount, err := strconv.Atoi(matches[1])
+		if err != nil {
+			dieCount = 1
+		}
+		sideCount, err := strconv.Atoi(matches[2])
+		if err != nil {
+			return nil, err
+		}
+
+		for range dieCount {
+			row.dice = append(row.dice, die{ sideCount: sideCount})
+		}
+	}
+	return &row, nil
+}
+
+func (r *row)SumRow() int {
+	sum := r.modifier
 	for _, die := range r.dice {
-		sum += die.Roll()
+		sum += die.value
 	}
 	return sum
+}
+
+func (r *row)Roll() {
+	for i := range r.dice {
+		r.dice[i].Roll()
+	}
 }
 
 func RollRows(rows *[]row) string {
 	res := ""
 	for _, row := range *rows {
-		res += strconv.Itoa(row.Roll())
+		row.Roll()
+		res += strconv.Itoa(row.SumRow())
 		res += "\n"
 	}
 	return res
@@ -39,24 +70,11 @@ func RollRows(rows *[]row) string {
 func ParseString(s string) ([]row, error) {
 	var rows []row
 	for _, line := range strings.Split(s, "\n") {
-		var row row
-		for _, diceGroup := range strings.Fields(line) {
-			re := regexp.MustCompile(`(\d*)d(\d+)`)
-			matches := re.FindStringSubmatch(diceGroup)
-			dieCount, err := strconv.Atoi(matches[1])
-			if err != nil {
-				dieCount = 1
-			}
-			sideCount, err := strconv.Atoi(matches[2])
-			if err != nil {
-				return nil, err
-			}
-
-			for range dieCount {
-				row.dice = append(row.dice, die{ sideCount: sideCount})
-			}
+		row, err := ParseRow(line)
+		if err != nil {
+			return nil, err
 		}
-		rows = append(rows, row)
+		rows = append(rows, *row)
 	}
 	return rows, nil
 }
