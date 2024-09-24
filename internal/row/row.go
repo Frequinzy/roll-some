7d10a6db058e6640e530444cf0dad19b861a8fe9
Emil Hultcrantz <emilhtz@gmail.com>
2024-09-24 22:29:14 +0200

    Add begin of BNF syntax to go version

--- internal/row/row.go
+++ internal/row/row.go
@@ -1,8 +1,10 @@
 package row
 
 import (
+	"fmt"
 	"math/rand"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -21,7 +23,116 @@ type row struct {
 	modifier int
 }
 
+
+// row = expr | expr, row | expr, operator, row ;
+// expr = modifier, action | action | int ;
+// action = "d", int | "(", row, ")" ;
+// modifier = low | high | int ;
+// low = "l", int | int, "l", int ;
+// high = "h", int | int, "h", int ;
+// operator = "+", | "-" ;
+type eRow struct {
+	exprs []expr
+	operators []operator
+}
+
+type expr struct {
+	action action
+	modifier modifier
+}
+
+type num int 
+
+type action interface {
+	calculate() int
+}
+
+func (r *eRow)calculate() int {
 	sum := 0
+	for i := range r.exprs {
+		e := r.exprs[i]
+		sum += e.modifier.modify(e.action)
+	}
+	fmt.Println(sum)
+	return sum
+}
+
+func (d *die)calculate() int {
+	d.Roll()
+	fmt.Println(d.value)
+	return d.value
+}
+
+func (n *num)calculate() int {
+	return int(*n)
+}
+
+type modifier interface {
+	modify(a action) int
+}
+
+type noMod struct {}
+
+func (n *noMod)modify(a action) int {
+	return a.calculate()
+}
+
+type multiplier int 
+
+func (m multiplier)modify(a action) int {
+	sum := 0
+	for range m {
+		sum += a.calculate()
+	}
+	return sum
+}
+
+type lowest struct {
+	pickN int
+	ofN int
+}
+
+func (l *lowest)modify(a action) int {
+	var results sort.IntSlice
+	for range l.ofN {
+		res := a.calculate()
+		results = append(results, res)
+	}
+
+	results.Sort()
+
+	sum := 0
+	for i := 0; i < l.pickN; {
+		sum += results[i]
+	}
+	return sum
+}
+
+type highest struct {
+	pickN int
+	ofN int
+}
+
+func (l *highest)modify(a action) int {
+	var results sort.IntSlice
+	for range l.ofN {
+		res := a.calculate()
+		results = append(results, res)
+	}
+
+	sort.Sort(sort.Reverse(results))
+
+	sum := 0
+	for i := range l.pickN {
+		sum += results[i]
+	}
+	return sum
+}
+
+type operator interface {
+	operate() int
+}
+
 func ParseRow(s string) (*row, error) {
 	var row row
 	for _, diceGroup := range strings.Fields(s) {
